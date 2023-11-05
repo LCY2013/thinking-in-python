787bd6a35b32ff99b2a8633f9ec6325ce3d55bd2
fufeng <15281205719@163.com>
2023-11-05 22:03:18 +0800

    Feat: parse html xpath

--- action/reptile/reptile-parse-html.py
+++ action/reptile/reptile-parse-html.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup as bs
 
 # bs4是第三方库需要使用pip命令安装
 
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
 
 header = {'user-agent': user_agent}
 
