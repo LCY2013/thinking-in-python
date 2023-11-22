538f2fd913a92a34aaee7b59ee730ed95bbc9bf3
扶风 <fufeng@staruniongame.com>
2023-11-22 12:19:09 +0800

    Feat: poll

---
+++ basic/base_eg/base_eg029_scheduling.py
@@ -0,0 +1,13 @@
+# 调度算法
+
+# 先来先服务(FCFS, First Come First Serve)
+# 短作业优先(SJF, Shortest Job First)
+# 最高优先权调度(Priority Scheduling)
+# 时间片轮转(RR, Round Robin)
+# 多级反馈队列调度(multilevel feedback queue scheduling)
+# 常见的调度算法总结:http://www.jianshu.com/p/6edf8174c1eb
+#
+# 实时调度算法:
+#
+# 最早截至时间优先 EDF
+# 最低松弛度优先 LLF
