0d9e64ff859162116835c1b118a118dcb5907bc7
扶风 <fufeng@staruniongame.com>
2023-12-01 18:54:07 +0800

    Feat: python class decorate

---
+++ basic/decorate/7class_decorate.py
@@ -0,0 +1,31 @@
+# Python 2.6 开始添加类装饰器
+from functools import wraps
+
+
+class MyClass(object):
+    def __init__(self, var='init_var', *args, **kwargs):
+        self._v = var
+        super(MyClass, self).__init__(*args, **kwargs)
+
+    def __call__(self, func):
+        # 类装饰器
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            func_name = func.__name__ + ' was called'
+            print(func_name)
+            return func(*args, **kwargs)
+
+        return wrapped_function
+
+
+def myfunc():
+    pass
+
+
+MyClass('100')(myfunc)()
+# 其他经常用在类装饰器的python自带装饰器
+# classmethod
+# staticmethod
+# property
+
+# 另一个示例
