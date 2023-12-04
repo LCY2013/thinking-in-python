a57f12bb7ed68031801c0ee4f11cee74ef3b6528
扶风 <fufeng@staruniongame.com>
2023-12-04 11:38:24 +0800

    Feat: python assign value

---
+++ basic/base_eg/base_eg032_assignment.py
@@ -0,0 +1,15 @@
+# 有时候我就是不想要默认值啊，只是想单单判断默认参数有没有值传递进来，那该怎么办？
+_no_value = object()
+
+
+def print_value(a, b=_no_value):
+    if b is _no_value:
+        print('not input b')
+        return
+    print('input b')
+    return
+
+
+print_value(1)
+print_value(1, 2)
+print_value(1, b=2)
