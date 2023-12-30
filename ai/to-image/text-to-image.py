0f4bed2e929cadaf8c699b6d93af1a434e9fcffe
fufeng <15281205719@163.com>
2023-12-30 12:33:34 +0800

    Feat: AI text to image

--- ai/to-image/text-to-image.py
+++ ai/to-image/text-to-image.py
@@ -2,9 +2,12 @@ from diffusers import AutoPipelineForText2Image
 import torch
 
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-pipe.to("cuda")
+# pipe.to("cuda")
+pipe.to("mps")
 
-prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+# prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+# prompt = "a photo of an astronaut riding a horse on mars"
+prompt = "A lazy idiot is riding a pig"
 
 image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
 
